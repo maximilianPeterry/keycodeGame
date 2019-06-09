2f2b776e576b81c9ab6db4ebc18b74f67e77ed47
Maximilian Riley <maximilianriley@Maximilians-MacBook-Pro.local>
2019-06-09 18:17:54 +0100

    keycodeNearlyDone

--- index.js
+++ index.js
@@ -25,35 +25,23 @@ document.addEventListener('keypress', () => {
     listWhich.textContent = event.keyCode;
     whichList.appendChild(listWhich)
 })
-// function keycode(){
-//     let key = event.keyCode;
-//     console.log(key);
-// }
 
-// myCode.addEventListener
-// resetObject.value(lists)
-
-// button.addEventListener('click', () => {
-//     let lists = document.getElementsByTagName('li'); //[0]
-//     for (let i = document.getElementsByClassName('lis'); i.lists > lists[6]; lists = lists[0]);
-// })
-    
-//get elements by tag name returns array so needs square brackets
-
-// if ((keyList.length + codeList.length + whichList.length) === 9) {
-// keyList.removeChild([1 && 2 && 3])
-// codeList.removeChild([1 && 2 && 3])
-// whichList.removeChild([1 && 2 && 3])
-// }
 
 const removeFromList = () => {
     // keyList.removeChild(keyList.childNodes[0]);
     // codeList.removeChild(codeList.childNodes[0]);
     // whichList.removeChild(whichList.childNodes[0]);
+    
     keyList.innerHTML = "";
     codeList.innerHTML = "";
     whichList.innerHTML = "";
 }
 button.onclick = function() {
     removeFromList();
+}
+
+let liList = document.getElementById('listKey').getElementsByTagName('li');
+let liGo = liList.length;
+if (liGo >= [3]) {
+    removeFromList()
 }
